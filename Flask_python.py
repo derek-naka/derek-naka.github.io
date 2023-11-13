a96aae28c7bd7bca126a565c3f2c031ce67bce39
Derek Nakagawa <101062797+derek-naka@users.noreply.github.com>
2023-11-12 23:26:41 -0800

    Add files via upload

---
+++ Flask_python.py
@@ -0,0 +1,38 @@
+from flask import Flask, redirect, url_for, request, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("Flask_html.html")
+
+
+def pass_fail_function(image):
+    return "val1", "val2"
+
+
+@app.route("/success", methods=["POST"])
+def success():
+    if request.method == "POST":
+        # Get the list of files from webpage
+        files = request.files.getlist("file")
+        len_files = len(files)
+
+        df = pd.DataFrame(columns=["file", "pass/fail", "fail reason"])
+
+        # Iterate for each file in the files List, and Save them
+        for file in files:
+            output = pass_fail_function(file)
+            df.loc[len(df.index)] = [file.filename, output[0], output[1]]
+            # file.save(file.filename)
+        return render_template(
+            "Flask_html_output.html",
+            tables=[df.to_html(classes="data")],
+            titles=df.columns.values,
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
